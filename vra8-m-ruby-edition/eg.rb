ff9a0cc3502d3d0d0fcaf9dae6fbaf8642e3c470
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2015-05-06 15:14:42 +0900

    Improve EG

--- vra8-m-ruby-edition/eg.rb
+++ vra8-m-ruby-edition/eg.rb
@@ -10,8 +10,8 @@ class EG
   LEVEL16_255         = 65534
 
   def initialize
-    @attack_interval  = $env_table_attack_interval_from_time[0]
-    @decay_interval   = $env_table_decay_interval_from_time[0]
+    @attack_interval  = $env_table_interval_from_time[0]
+    @decay_interval   = $env_table_interval_from_time[0]
     @sustain_level_16 = 127
     @state            = STATE_IDLE
     @level_16         = 0
@@ -19,11 +19,11 @@ class EG
   end
 
   def set_attack_time(attack_time)
-    @attack_interval = $env_table_attack_interval_from_time[attack_time]
+    @attack_interval = $env_table_interval_from_time[attack_time]
   end
 
   def set_decay_time(decay_time)
-    @decay_interval = $env_table_decay_interval_from_time[decay_time]
+    @decay_interval = $env_table_interval_from_time[decay_time]
   end
 
   def set_sustain_level(sustain_level)
@@ -55,7 +55,8 @@ class EG
       end
       @count = 0
 
-      @level_16 = LEVEL16_255 - muls_16(LEVEL16_255 - @level_16, EG_CHANGE_FACTOR)
+      @level_16 = LEVEL16_255 -
+                  muls_16(LEVEL16_255 - @level_16, ENV_ATTACK_FACTOR)
       if (@level_16 >= LEVEL16_127)
         @state = STATE_DECAY_SUSTAIN
         @level_16 = LEVEL16_127
@@ -71,7 +72,8 @@ class EG
         if (@level_16 <= (32 + @sustain_level_16))
           @level_16 = @sustain_level_16
         elsif
-          @level_16 = @sustain_level_16 + muls_16(@level_16 - @sustain_level_16, EG_CHANGE_FACTOR)
+          @level_16 = @sustain_level_16 +
+                      muls_16(@level_16 - @sustain_level_16, ENV_DECAY_FACTOR)
         end
       end
     when STATE_RELEASE
@@ -81,7 +83,7 @@ class EG
       end
       @count = 0
 
-      @level_16 = muls_16(@level_16, EG_CHANGE_FACTOR)
+      @level_16 = muls_16(@level_16, ENV_DECAY_FACTOR)
       if (@level_16 <= 32)
         @state = STATE_IDLE
         @level_16 = 0
--- vra8-m-ruby-edition/generate-env-table.rb
+++ vra8-m-ruby-edition/generate-env-table.rb
@@ -2,31 +2,19 @@ require './common'
 
 $file = File::open("env-table.rb", "w")
 
-$file.printf("$env_table_attack_interval_from_time = [\n  ")
+env_attack_factor = (((1.0 / 2.0) ** (1.0 / 160.0)) * 65536.0).round
+env_decay_factor = (((1.0 / 2.0) ** (1.0 / 32.0)) * 65536.0).round
+
+$file.printf("ENV_ATTACK_FACTOR = %d\n", env_attack_factor)
+$file.printf("ENV_DECAY_FACTOR = %d\n", env_decay_factor)
+$file.printf("\n")
+
+$file.printf("$env_table_interval_from_time = [\n  ")
 (0..127).each do |time|
   t = [time, 32].max
   sec = 10.0 / (10.0 ** ((128.0 - t) / (128.0 / 4.0)))
   interval = (sec * SAMPLING_RATE) / (Math.log(1.0 / 2.0) /
-                                      Math.log(EG_CHANGE_FACTOR / 65536.0))
-
-  r = interval.round.to_i
-  $file.printf("%5d,", r)
-  if time == 127
-    $file.printf("\n")
-  elsif time % 16 == 15
-    $file.printf("\n  ")
-  else
-    $file.printf(" ")
-  end
-end
-$file.printf("]\n\n")
-
-$file.printf("$env_table_decay_interval_from_time = [\n  ")
-(0..127).each do |time|
-  t = [time, 32].max
-  sec = 10.0 / (10.0 ** ((128.0 - t) / (128.0 / 4.0)))
-  interval = (sec * SAMPLING_RATE) / (Math.log(1.0 / 32.0) /
-                                      Math.log(EG_CHANGE_FACTOR / 65536.0))
+                                      Math.log(env_attack_factor / 65536.0))
 
   r = interval.round.to_i
   $file.printf("%5d,", r)
