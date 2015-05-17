b72d23d234a5be940ccca2a5cbdbc1d460889071
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2015-05-17 19:41:09 +0900

    Improve LPF

--- vra8-m-ruby-edition/generate-lpf-table.rb
+++ vra8-m-ruby-edition/generate-lpf-table.rb
@@ -5,7 +5,7 @@ $file = File::open("lpf-table.rb", "w")
 def generate_lpf_table(name, q)
   $file.printf("$lpf_table_%s = [\n  ", name)
   (0..127).each do |i|
-    f0_over_fs = (2.0 ** (i / (128.0 / 8.0))) / (2.0 ** 9.0)
+    f0_over_fs = (2.0 ** (i / (128.0 / 6.0))) / (2.0 ** 7.0)
 
     w0 = 2.0 * Math::PI * f0_over_fs
     alpha = Math::sin(w0) / (2.0 * q)
