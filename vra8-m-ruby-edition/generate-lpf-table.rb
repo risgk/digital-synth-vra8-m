b90933beaee65a87d72eacadb3f6edac991d5462
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2015-05-17 20:00:25 +0900

    Improve LPF

--- vra8-m-ruby-edition/generate-lpf-table.rb
+++ vra8-m-ruby-edition/generate-lpf-table.rb
@@ -5,7 +5,7 @@ $file = File::open("lpf-table.rb", "w")
 def generate_lpf_table(name, q)
   $file.printf("$lpf_table_%s = [\n  ", name)
   (0..127).each do |i|
-    f0_over_fs = (2.0 ** (i / (128.0 / 6.0))) / (2.0 ** 7.0)
+    f0_over_fs = (2.0 ** (i / (127.0 / 6.0))) / (2.0 ** 7.0)
 
     w0 = 2.0 * Math::PI * f0_over_fs
     alpha = Math::sin(w0) / (2.0 * q)
@@ -15,11 +15,12 @@ def generate_lpf_table(name, q)
     a1 = (-2.0) * Math::cos(w0)
     a2 = 1.0 - alpha
 
-    b2_over_a0 = ((b2 / a0) * LPF_TABLE_ONE).round.to_i
-    a1_over_a0 = ((a1 / a0) * LPF_TABLE_ONE).round.to_i
-    a2_over_a0 = (b2_over_a0 * 4) - a1_over_a0 - LPF_TABLE_ONE
+    lpf_table_one = 2 ** LPF_TABLE_BITS
+    b2_over_a0   = +((b2 / a0) * lpf_table_one).round.to_i
+    a1_over_a0_i = -((a1 / a0) * lpf_table_one).round.to_i
+    a2_over_a0   = +(b2_over_a0 * 4) + a1_over_a0_i - lpf_table_one
 
-    $file.printf("%+6d, %+6d, %+6d,", b2_over_a0, a1_over_a0, a2_over_a0)
+    $file.printf("%+6d, %+6d, %+6d,", b2_over_a0, a1_over_a0_i, a2_over_a0)
     if i == 127
       $file.printf("\n")
     elsif i % 4 == 3
--- vra8-m-ruby-edition/vcf.rb
+++ vra8-m-ruby-edition/vcf.rb
@@ -36,22 +36,22 @@ class VCF
 
     if ((@resonance & 0x40) != 0)
       i = cutoff * 3
-      b2_over_a0 = $lpf_table_q_2_sqrt_2[i + 0]
-      a1_over_a0 = $lpf_table_q_2_sqrt_2[i + 1]
-      a2_over_a0 = $lpf_table_q_2_sqrt_2[i + 2]
+      b2_over_a0   = $lpf_table_q_2_sqrt_2[i + 0]
+      a1_over_a0_i = $lpf_table_q_2_sqrt_2[i + 1]
+      a2_over_a0   = $lpf_table_q_2_sqrt_2[i + 2]
     else
       i = cutoff * 3
-      b2_over_a0 = $lpf_table_q_1_over_sqrt_2[i + 0]
-      a1_over_a0 = $lpf_table_q_1_over_sqrt_2[i + 1]
-      a2_over_a0 = $lpf_table_q_1_over_sqrt_2[i + 2]
+      b2_over_a0   = $lpf_table_q_1_over_sqrt_2[i + 0]
+      a1_over_a0_i = $lpf_table_q_1_over_sqrt_2[i + 1]
+      a2_over_a0   = $lpf_table_q_1_over_sqrt_2[i + 2]
     end
 
     x0 = a_in << 8
     r = x0 + (@x1 << 1) + @x2
-    tmp  = muls_h16(b2_over_a0, r)
-    tmp -= muls_h16(a1_over_a0, @y1)
-    tmp -= muls_h16(a2_over_a0, @y2)
-    y0 = tmp << (0x8000 / LPF_TABLE_ONE)
+    tmp  = muls_h16(b2_over_a0,   r)
+    tmp += muls_h16(a1_over_a0_i, @y1)
+    tmp -= muls_h16(a2_over_a0,   @y2)
+    y0 = tmp << (16 - LPF_TABLE_BITS)
     @x2 = @x1
     @y2 = @y1
     @x1 = x0
