ce88394f542be3ce8810860990788d5fa26ec92c
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2015-05-25 21:12:49 +0900

    Improve Portamento

--- vra8-m-ruby-edition/slew-limiter.rb
+++ vra8-m-ruby-edition/slew-limiter.rb
@@ -2,6 +2,7 @@ require_relative 'common'
 
 class SlewLimiter
   def initialize
+    @count = 0
     @level = NOTE_NUMBER_MIN << 8
     set_slew_time(NOTE_NUMBER_MIN)
   end
@@ -11,13 +12,19 @@ class SlewLimiter
   end
 
   def clock(input)
-    if (@level > input + @slew_rate)
-      @level -= @slew_rate
-    elsif (@level < input - @slew_rate)
-      @level += @slew_rate
-    else
-      @level = input
+    @count += 1
+    if (@count >= 1)
+      @count = 0
+
+      if (@level > input + @slew_rate)
+        @level -= @slew_rate
+      elsif (@level < input - @slew_rate)
+        @level += @slew_rate
+      else
+        @level = input
+      end
     end
+
     return @level
   end
 end
