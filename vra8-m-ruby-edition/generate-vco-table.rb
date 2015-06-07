f22153210fda44b61f0006c4ed548afc66e1f9bc
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2015-06-07 20:24:47 +0900

    Improve code

--- vra8-m-ruby-edition/generate-vco-table.rb
+++ vra8-m-ruby-edition/generate-vco-table.rb
@@ -4,15 +4,20 @@ $file = File.open("vco-table.rb", "w")
 
 $vco_freq_table = []
 
+def freq_from_note_number(note_number)
+  cent = (note_number * 100.0) - 6900.0
+  hz = 440.0 * (2.0 ** (cent / 1200.0))
+  freq = (hz * VCO_PHASE_RESOLUTION / SAMPLING_RATE * 2.0).floor
+  freq = freq + 1 if freq.even?
+  freq
+end
+
 $file.printf("$vco_freq_table = [\n  ")
 (0..DATA_BYTE_MAX).each do |note_number|
   if (note_number < NOTE_NUMBER_MIN) || (note_number > NOTE_NUMBER_MAX)
     freq = 0
   else
-    cent = (note_number * 100.0) - 6900.0
-    hz = 440.0 * (2.0 ** (cent / 1200.0))
-    freq = (hz * VCO_PHASE_RESOLUTION / SAMPLING_RATE * 2.0).floor
-    freq = freq + 1 if freq.even?
+    freq = freq_from_note_number(note_number)
   end
   $vco_freq_table << freq
 
@@ -72,22 +77,27 @@ def generate_vco_wave_table_sawtooth(max)
   end
 end
 
-$vco_freq_restriction_table = $vco_freq_table.dup
-$vco_freq_restriction_table.shift
-$vco_freq_restriction_table.push(0)
-$vco_freq_restriction_table[NOTE_NUMBER_MIN - 1] = 0
-$vco_freq_restriction_table[NOTE_NUMBER_MAX] = $vco_freq_restriction_table[NOTE_NUMBER_MAX - 1]
+$vco_overtones_restriction_table = []
+
+(0..DATA_BYTE_MAX).each do |note_number|
+  if (note_number < NOTE_NUMBER_MIN) || (note_number > NOTE_NUMBER_MAX)
+    freq = 0
+  else
+    freq = freq_from_note_number(note_number + 1)
+  end
+  $vco_overtones_restriction_table << freq
+end
 
 def max_overtone(freq)
   (freq != 0) ? ((FREQUENCY_MAX * VCO_PHASE_RESOLUTION) / ((freq / 2) * SAMPLING_RATE)) : 0
 end
 
-$vco_freq_restriction_table.map { |freq| max_overtone(freq) }.uniq.sort.reverse.each do |i|
+$vco_overtones_restriction_table.map { |freq| max_overtone(freq) }.uniq.sort.reverse.each do |i|
   generate_vco_wave_table_sawtooth(i) if i != -1
 end
 
 $file.printf("$vco_wave_tables = [\n  ")
-$vco_freq_restriction_table.each_with_index do |freq, idx|
+$vco_overtones_restriction_table.each_with_index do |freq, idx|
   $file.printf("$vco_wave_table_%-3d,", max_overtone(freq))
   if idx == DATA_BYTE_MAX
     $file.printf("\n")
