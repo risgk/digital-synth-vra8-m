99224c7ae043b2313714fd54000d219badc2597d
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2015-07-19 16:33:54 +0900

    Change EG Time

--- DigitalSynthVRA8M/generate-eg-table.rb
+++ DigitalSynthVRA8M/generate-eg-table.rb
@@ -4,14 +4,18 @@ $file = File.open("eg-table.h", "w")
 
 $file.printf("#pragma once\n\n")
 
-$file.printf("const uint16_t g_eg_attack_rate_table[] PROGMEM = {\n  ")
+EG_ATTACK_UPDATE_INTERVAL = 2
+$file.printf("const uint8_t EG_ATTACK_UPDATE_INTERVAL = %d;\n", EG_ATTACK_UPDATE_INTERVAL)
+$file.printf("\n")
+
+$file.printf("const uint16_t g_eg_attack_rate_table[] = {\n  ")
 (0..((1 << EG_CONTROLLER_STEPS_BITS) - 1)).each do |time|
   t = time
   t = 1 if t == 0
-  sec = (EG_LEVEL_MAX.to_f / SAMPLING_RATE) /
+  sec = (EG_LEVEL_MAX.to_f / SAMPLING_RATE) * EG_ATTACK_UPDATE_INTERVAL /
         (10.0 ** ((((1 << EG_CONTROLLER_STEPS_BITS) - 1) - t) /
                   (((1 << EG_CONTROLLER_STEPS_BITS) - 2) / 3.0))) / 2.0
-  rate = (EG_LEVEL_MAX / (sec * SAMPLING_RATE)).round.to_i
+  rate = (EG_LEVEL_MAX * EG_ATTACK_UPDATE_INTERVAL / (sec * SAMPLING_RATE)).round.to_i
 
   $file.printf("%5d,", rate)
   if time == DATA_BYTE_MAX
@@ -24,22 +28,60 @@ $file.printf("const uint16_t g_eg_attack_rate_table[] PROGMEM = {\n  ")
 end
 $file.printf("};\n\n")
 
-HALF_LIFE = 16
-eg_decay_release_rate = (((1.0 / 2.0) ** (1.0 / HALF_LIFE)) *
-                         (1 << EG_DECAY_RELEASE_RATE_DENOMINATOR_BITS)).round
-$file.printf("const uint16_t EG_DECAY_RELEASE_RATE = %d;\n", eg_decay_release_rate)
-$file.printf("\n")
+def decay_release_rate(t)
+  case t
+  when 0..11
+    rate = (((1.0 / 2.0) ** (1.0 / 4.0))  *
+            (1 << EG_DECAY_RELEASE_RATE_DENOMINATOR_BITS)).round
+  when 12..21
+    rate = (((1.0 / 2.0) ** (1.0 / 16.0)) *
+            (1 << EG_DECAY_RELEASE_RATE_DENOMINATOR_BITS)).round
+  when 22..31
+    rate = (((1.0 / 2.0) ** (1.0 / 32.0)) *
+            (1 << EG_DECAY_RELEASE_RATE_DENOMINATOR_BITS)).round
+  end
+  rate
+end
 
-$file.printf("const uint16_t g_eg_decay_release_update_interval_table[] PROGMEM = {\n  ")
+def decay_release_update_interval(t)
+  sec = 10.0 / (10.0 ** ((((1 << EG_CONTROLLER_STEPS_BITS) - 1) - t) /
+                         (((1 << EG_CONTROLLER_STEPS_BITS) - 2) / 3.0)))
+
+  # error correction
+  case t
+  when 22..31
+    sec = sec * 1.25
+  end
+
+  update_interval = ((sec * SAMPLING_RATE) /
+                     (Math.log(1.0 / 256.0) /
+                      Math.log(decay_release_rate(t).to_f /
+                      (1 << EG_DECAY_RELEASE_RATE_DENOMINATOR_BITS)))
+                    ).round.to_i
+end
+
+$file.printf("const uint8_t g_eg_decay_release_rate_table[] = {\n  ")
 (0..((1 << EG_CONTROLLER_STEPS_BITS) - 1)).each do |time|
   t = time
   t = 1 if t == 0
-  sec = 12.8 / (10.0 ** ((((1 << EG_CONTROLLER_STEPS_BITS) - 1) - t) /
-                         (((1 << EG_CONTROLLER_STEPS_BITS) - 2) / 3.0)))
-  update_interval = ((sec * SAMPLING_RATE) /
-                     (Math.log(1.0 / 1024.0) /
-                      Math.log(eg_decay_release_rate.to_f / (1 << EG_DECAY_RELEASE_RATE_DENOMINATOR_BITS)))
-                    ).round.to_i
+  rate = decay_release_rate(t)
+
+  $file.printf("%5d,", rate)
+  if time == DATA_BYTE_MAX
+    $file.printf("\n")
+  elsif time % 16 == 15
+    $file.printf("\n  ")
+  else
+    $file.printf(" ")
+  end
+end
+$file.printf("};\n\n")
+
+$file.printf("const uint16_t g_eg_decay_release_update_interval_table[] = {\n  ")
+(0..((1 << EG_CONTROLLER_STEPS_BITS) - 1)).each do |time|
+  t = time
+  t = 1 if t == 0
+  update_interval = decay_release_update_interval(t)
 
   $file.printf("%5d,", update_interval)
   if time == DATA_BYTE_MAX
--- vra8-m-ruby-edition/eg.rb
+++ vra8-m-ruby-edition/eg.rb
@@ -10,7 +10,7 @@ class EG
 
   def initialize
     @state = STATE_IDLE
-    @decay_release_count = 0
+    @count = 0
     @level = 0
     set_attack(0)
     set_decay_release(0)
@@ -22,8 +22,9 @@ class EG
   end
 
   def set_decay_release(controller_value)
-    @decay_release_update_interval =
-      $eg_decay_release_update_interval_table[controller_value >> (7 - EG_CONTROLLER_STEPS_BITS)]
+    time = controller_value >> (7 - EG_CONTROLLER_STEPS_BITS)
+    @eg_decay_release_rate         = $eg_decay_release_rate_table[time]
+    @decay_release_update_interval = $eg_decay_release_update_interval_table[time]
   end
 
   def set_sustain(controller_value)
@@ -32,45 +33,49 @@ class EG
 
   def note_on
     @state = STATE_ATTACK
-    @decay_release_count = 0
+    @count = 0
   end
 
   def note_off
     @state = STATE_RELEASE
-    @decay_release_count = 0
+    @count = 0
   end
 
   def clock
     case (@state)
     when STATE_ATTACK
-      if (@level >= EG_LEVEL_MAX - @attack_rate)
-        @state = STATE_DECAY_SUSTAIN
-        @level = EG_LEVEL_MAX
-      else
-        @level += @attack_rate
+      @count += 1
+      if (@count >= EG_ATTACK_UPDATE_INTERVAL)
+        @count = 0
+        if (@level >= EG_LEVEL_MAX - @attack_rate)
+          @state = STATE_DECAY_SUSTAIN
+          @level = EG_LEVEL_MAX
+        else
+          @level += @attack_rate
+        end
       end
     when STATE_DECAY_SUSTAIN
-      @decay_release_count += 1
-      if (@decay_release_count >= @decay_release_update_interval)
-        @decay_release_count = 0
+      @count += 1
+      if (@count >= @decay_release_update_interval)
+        @count = 0
         if (@level > @sustain_level)
           if (@level <= @sustain_level + (EG_LEVEL_MAX >> 10))
             @level = @sustain_level
           else
             @level = @sustain_level +
-                     mul_q16_q8(@level - @sustain_level, EG_DECAY_RELEASE_RATE)
+                     mul_q16_q8(@level - @sustain_level, @eg_decay_release_rate)
           end
         end
       end
     when STATE_RELEASE
-      @decay_release_count += 1
-      if (@decay_release_count >= @decay_release_update_interval)
-        @decay_release_count = 0
+      @count += 1
+      if (@count >= @decay_release_update_interval)
+        @count = 0
         if (@level <= (EG_LEVEL_MAX >> 10))
           @state = STATE_IDLE
           @level = 0
         else
-          @level = mul_q16_q8(@level, EG_DECAY_RELEASE_RATE)
+          @level = mul_q16_q8(@level, @eg_decay_release_rate)
         end
       end
     when STATE_IDLE
